3ed8333673284e000c4dd70f8c46864545ea44bf
Jeff Reiher <jeffreiher@gmail.com>
2015-04-15 16:57:32 -0400

    add for loops

--- restaurant/add_items_to_database.py
+++ restaurant/add_items_to_database.py
@@ -1,22 +1,27 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restuarant, MenuItem
+from database_setup import Base, Restaurant, MenuItem
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
-myFirstRestaurant = Restaurant(name = "Pizza Palace")
-session.add(myFirstRestaurant)
-session.commit()
-session.query(Restaurant).all()
+# myFirstRestaurant = Restaurant(name = "Pizza Palace")
+# session.add(myFirstRestaurant)
+# session.commit()
+# session.query(Restaurant).all()
 #[<database_setup.Restaurant object at 0xb670070c>]
 
-cheesepizza = MenuItem(name = "Cheese Pizza", description = "Made with all\
-natural ingredients and fresh mozzarella", course = "Entree", price = "$8.99",
-restaurant = myFirstRestaurant)
+# cheesepizza = MenuItem(name = "Cheese Pizza", description = "Made with all\
+# natural ingredients and fresh mozzarella", course = "Entree", price = "$8.99",
+# restaurant = myFirstRestaurant)
 
-session.add(cheesepizza)
-session.commit()
-session.query(menuItem).all()
+# session.add(cheesepizza)
+# session.commit()
+# print session.query(menuItem).all()
+
+#### for loops ####
+items = session.query(MenuItem).all()
+for item in items:
+	print item.name, item.price
