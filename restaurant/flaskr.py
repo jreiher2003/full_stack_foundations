4e76d006c3c3665781a70314fc261bf842281d9f
Jeff Reiher <jeffreiher@gmail.com>
2015-04-19 10:13:17 -0400

    add db imports for sqlalc

--- restaurant/flaskr.py
+++ restaurant/flaskr.py
@@ -1,6 +1,17 @@
 from flask import Flask 
 app = Flask(__name__)
 
+# step 1 sqla db code import
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('splite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 # @app.route('/')
 @app.route('/hello')
 def helloWorld():
