333cabe59c3b5acda7ef23b79a53e9c53f5eca3f
Jeff Reiher <jeffreiher@gmail.com>
2015-04-20 11:55:46 -0400

    removed spelling errors

--- restaurant/flaskr.py
+++ restaurant/flaskr.py
@@ -6,13 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
-engine = create_engine('splite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# @app.route('/')
+@app.route('/')
 @app.route('/hello')
 def helloWorld():
 	return "Hello World"
