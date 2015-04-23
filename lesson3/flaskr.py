496948cb83d264a681f34038acd283cb6f1829dc
Jeff Reiher <jeffreiher@gmail.com>
2015-04-23 10:05:38 -0400

    add deleteMenuItem functionality to code

--- lesson3/flaskr.py
+++ lesson3/flaskr.py
@@ -55,6 +55,10 @@ def newMenuItem(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
 	editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+	
+	if request.method == 'GET':
+		return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, i=editedItem)
+		
 	if request.method == 'POST':
 		if request.form['name']:
 			editedItem.name = request.form['name']
@@ -63,14 +67,18 @@ def editMenuItem(restaurant_id, menu_id):
 		return redirect(url_for('restaurantMenuItems', restaurant_id=restaurant_id))
 	
 
-	if request.method == 'GET':
-		return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, i=editedItem)
     
 
 #Task 3: Create a route for deleteMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET','POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+	deleteItem = session.query(MenuItem).filter_by(id=menu_id).one()
+	if request.method == 'GET':
+		return render_template('deletemenuitem.html',restaurant_id=restaurant_id, i=deleteItem)
+	if request.method == 'POST':
+		session.delete(deleteItem)
+		session.commit()
+		return redirect(url_for('restaurantMenuItems', restaurant_id=restaurant_id))
 
 	
 if __name__ == '__main__':
